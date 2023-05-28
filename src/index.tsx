d38c780fe22430a513089965793012f4aa5a6103
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-05-28 13:30:04 -0700

    pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -10,7 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
